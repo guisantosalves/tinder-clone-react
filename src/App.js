18a3f6405ee174ad795e09ccff21fec64edf3b91
guisantosalves <workguisantos@gmail.com>
2022-06-27 16:01:47 -0400

    inserting react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Chat from "./Chat";
 
 function App() {
   return (
     <div className="App">
-
       {/* header */}
-      <Header/>
-
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/chat" element={<Chat/>}/>
+      </Routes>
       {/* tinder cards */}
       {/* buttons */}
 
