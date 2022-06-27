6ccf3938d50639b981ae8f1750a0115a8d6bc85c
guisantosalves <workguisantos@gmail.com>
2022-06-27 15:09:35 -0400

    starting the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's builder tinder clone</h1>
+
+      {/* header */}
+      {/* tinder cards */}
+      {/* buttons */}
+    </div>
+  );
+}
+
+export default App;
